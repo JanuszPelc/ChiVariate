c757c4ed4f21c07d4e65aea596cd900e16cfcc80
Janusz Pelc <jp@onet.pl>
2026-01-09 16:00:42 +0100

    Refactor benchmarks: simplify categories, unify configurations, and add missing double-equivalent methods.

--- csharp/benchmarks/ChiVariate.Benchmarks/FixedBenchmarks.cs
+++ csharp/benchmarks/ChiVariate.Benchmarks/FixedBenchmarks.cs
@@ -1,4 +1,5 @@
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
@@ -8,13 +9,14 @@ namespace ChiVariate.Benchmarks;
 [SimpleJob]
 [MinIterationTime(500)]
 [IterationCount(15)]
+[GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
 public class FixedBenchmarks
 {
     private const int Ops = 1_000;
     private static readonly ChiFixed Ten = (ChiFixed)10m;
+
     private double[] _doubleValues = null!;
     private double[] _doubleValues2 = null!;
-
     private ChiFixed[] _fixedValues = null!;
     private ChiFixed[] _fixedValues2 = null!;
 
@@ -41,38 +43,10 @@ public class FixedBenchmarks
         }
     }
 
-    #region Power - ChiFixed
+    #region Arithmetic
 
     [Benchmark]
-    [BenchmarkCategory("Power", "ChiFixed")]
-    public ChiFixed Fixed_Pow()
-    {
-        var result = ChiFixed.Zero;
-        for (var i = 0; i < Ops; i++)
-            result = ChiFixed.Pow(ChiFixed.Abs(_fixedValues[i]) + ChiFixed.One, _fixedValues2[i] / Ten);
-        return result;
-    }
-
-    #endregion
-
-    #region Power - Double
-
-    [Benchmark]
-    [BenchmarkCategory("Power", "Double")]
-    public double Double_Pow()
-    {
-        var result = 0.0;
-        for (var i = 0; i < Ops; i++)
-            result = Math.Pow(Math.Abs(_doubleValues[i]) + 1.0, _doubleValues2[i] / 10.0);
-        return result;
-    }
-
-    #endregion
-
-    #region Arithmetic - ChiFixed
-
-    [Benchmark]
-    [BenchmarkCategory("Arithmetic", "ChiFixed")]
+    [BenchmarkCategory("Arithmetic")]
     public ChiFixed Fixed_Add()
     {
         var sum = ChiFixed.Zero;
@@ -82,51 +56,7 @@ public class FixedBenchmarks
     }
 
     [Benchmark]
-    [BenchmarkCategory("Arithmetic", "ChiFixed")]
-    public ChiFixed Fixed_Sub()
-    {
-        var result = ChiFixed.Zero;
-        for (var i = 0; i < Ops; i++)
-            result -= _fixedValues[i];
-        return result;
-    }
-
-    [Benchmark]
-    [BenchmarkCategory("Arithmetic", "ChiFixed")]
-    public ChiFixed Fixed_Mul()
-    {
-        var result = ChiFixed.One;
-        for (var i = 0; i < Ops; i++)
-            result = _fixedValues[i] * _fixedValues2[i];
-        return result;
-    }
-
-    [Benchmark]
-    [BenchmarkCategory("Arithmetic", "ChiFixed")]
-    public ChiFixed Fixed_Div()
-    {
-        var result = ChiFixed.One;
-        for (var i = 0; i < Ops; i++)
-            result = _fixedValues[i] / _fixedValues2[i];
-        return result;
-    }
-
-    [Benchmark]
-    [BenchmarkCategory("Arithmetic", "ChiFixed")]
-    public ChiFixed Fixed_Mod()
-    {
-        var result = ChiFixed.Zero;
-        for (var i = 0; i < Ops; i++)
-            result = _fixedValues[i] % _fixedValues2[i];
-        return result;
-    }
-
-    #endregion
-
-    #region Arithmetic - Double
-
-    [Benchmark]
-    [BenchmarkCategory("Arithmetic", "Double")]
+    [BenchmarkCategory("Arithmetic")]
     public double Double_Add()
     {
         var sum = 0.0;
@@ -136,7 +66,17 @@ public class FixedBenchmarks
     }
 
     [Benchmark]
-    [BenchmarkCategory("Arithmetic", "Double")]
+    [BenchmarkCategory("Arithmetic")]
+    public ChiFixed Fixed_Sub()
+    {
+        var result = ChiFixed.Zero;
+        for (var i = 0; i < Ops; i++)
+            result -= _fixedValues[i];
+        return result;
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Arithmetic")]
     public double Double_Sub()
     {
         var result = 0.0;
@@ -146,7 +86,17 @@ public class FixedBenchmarks
     }
 
     [Benchmark]
-    [BenchmarkCategory("Arithmetic", "Double")]
+    [BenchmarkCategory("Arithmetic")]
+    public ChiFixed Fixed_Mul()
+    {
+        var result = ChiFixed.One;
+        for (var i = 0; i < Ops; i++)
+            result = _fixedValues[i] * _fixedValues2[i];
+        return result;
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Arithmetic")]
     public double Double_Mul()
     {
         var result = 1.0;
@@ -156,7 +106,17 @@ public class FixedBenchmarks
     }
 
     [Benchmark]
-    [BenchmarkCategory("Arithmetic", "Double")]
+    [BenchmarkCategory("Arithmetic")]
+    public ChiFixed Fixed_Div()
+    {
+        var result = ChiFixed.One;
+        for (var i = 0; i < Ops; i++)
+            result = _fixedValues[i] / _fixedValues2[i];
+        return result;
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Arithmetic")]
     public double Double_Div()
     {
         var result = 1.0;
@@ -166,7 +126,17 @@ public class FixedBenchmarks
     }
 
     [Benchmark]
-    [BenchmarkCategory("Arithmetic", "Double")]
+    [BenchmarkCategory("Arithmetic")]
+    public ChiFixed Fixed_Mod()
+    {
+        var result = ChiFixed.Zero;
+        for (var i = 0; i < Ops; i++)
+            result = _fixedValues[i] % _fixedValues2[i];
+        return result;
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Arithmetic")]
     public double Double_Mod()
     {
         var result = 0.0;
@@ -177,10 +147,10 @@ public class FixedBenchmarks
 
     #endregion
 
-    #region Rounding - ChiFixed
+    #region Rounding
 
     [Benchmark]
-    [BenchmarkCategory("Rounding", "ChiFixed")]
+    [BenchmarkCategory("Rounding")]
     public ChiFixed Fixed_Floor()
     {
         var result = ChiFixed.Zero;
@@ -190,41 +160,7 @@ public class FixedBenchmarks
     }
 
     [Benchmark]
-    [BenchmarkCategory("Rounding", "ChiFixed")]
-    public ChiFixed Fixed_Ceiling()
-    {
-        var result = ChiFixed.Zero;
-        for (var i = 0; i < Ops; i++)
-            result = ChiFixed.Ceiling(_fixedValues[i]);
-        return result;
-    }
-
-    [Benchmark]
-    [BenchmarkCategory("Rounding", "ChiFixed")]
-    public ChiFixed Fixed_Truncate()
-    {
-        var result = ChiFixed.Zero;
-        for (var i = 0; i < Ops; i++)
-            result = ChiFixed.Truncate(_fixedValues[i]);
-        return result;
-    }
-
-    [Benchmark]
-    [BenchmarkCategory("Rounding", "ChiFixed")]
-    public ChiFixed Fixed_Round()
-    {
-        var result = ChiFixed.Zero;
-        for (var i = 0; i < Ops; i++)
-            result = ChiFixed.Round(_fixedValues[i]);
-        return result;
-    }
-
-    #endregion
-
-    #region Rounding - Double
-
-    [Benchmark]
-    [BenchmarkCategory("Rounding", "Double")]
+    [BenchmarkCategory("Rounding")]
     public double Double_Floor()
     {
         var result = 0.0;
@@ -234,7 +170,17 @@ public class FixedBenchmarks
     }
 
     [Benchmark]
-    [BenchmarkCategory("Rounding", "Double")]
+    [BenchmarkCategory("Rounding")]
+    public ChiFixed Fixed_Ceiling()
+    {
+        var result = ChiFixed.Zero;
+        for (var i = 0; i < Ops; i++)
+            result = ChiFixed.Ceiling(_fixedValues[i]);
+        return result;
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Rounding")]
     public double Double_Ceiling()
     {
         var result = 0.0;
@@ -244,7 +190,17 @@ public class FixedBenchmarks
     }
 
     [Benchmark]
-    [BenchmarkCategory("Rounding", "Double")]
+    [BenchmarkCategory("Rounding")]
+    public ChiFixed Fixed_Truncate()
+    {
+        var result = ChiFixed.Zero;
+        for (var i = 0; i < Ops; i++)
+            result = ChiFixed.Truncate(_fixedValues[i]);
+        return result;
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Rounding")]
     public double Double_Truncate()
     {
         var result = 0.0;
@@ -254,7 +210,17 @@ public class FixedBenchmarks
     }
 
     [Benchmark]
-    [BenchmarkCategory("Rounding", "Double")]
+    [BenchmarkCategory("Rounding")]
+    public ChiFixed Fixed_Round()
+    {
+        var result = ChiFixed.Zero;
+        for (var i = 0; i < Ops; i++)
+            result = ChiFixed.Round(_fixedValues[i]);
+        return result;
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Rounding")]
     public double Double_Round()
     {
         var result = 0.0;
@@ -265,10 +231,10 @@ public class FixedBenchmarks
 
     #endregion
 
-    #region Roots - ChiFixed
+    #region Roots
 
     [Benchmark]
-    [BenchmarkCategory("Roots", "ChiFixed")]
+    [BenchmarkCategory("Roots")]
     public ChiFixed Fixed_Sqrt()
     {
         var result = ChiFixed.Zero;
@@ -278,21 +244,7 @@ public class FixedBenchmarks
     }
 
     [Benchmark]
-    [BenchmarkCategory("Roots", "ChiFixed")]
-    public ChiFixed Fixed_Cbrt()
-    {
-        var result = ChiFixed.Zero;
-        for (var i = 0; i < Ops; i++)
-            result = ChiFixed.Cbrt(_fixedValues[i]);
-        return result;
-    }
-
-    #endregion
-
-    #region Roots - Double
-
-    [Benchmark]
-    [BenchmarkCategory("Roots", "Double")]
+    [BenchmarkCategory("Roots")]
     public double Double_Sqrt()
     {
         var result = 0.0;
@@ -302,7 +254,17 @@ public class FixedBenchmarks
     }
 
     [Benchmark]
-    [BenchmarkCategory("Roots", "Double")]
+    [BenchmarkCategory("Roots")]
+    public ChiFixed Fixed_Cbrt()
+    {
+        var result = ChiFixed.Zero;
+        for (var i = 0; i < Ops; i++)
+            result = ChiFixed.Cbrt(_fixedValues[i]);
+        return result;
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Roots")]
     public double Double_Cbrt()
     {
         var result = 0.0;
@@ -313,10 +275,34 @@ public class FixedBenchmarks
 
     #endregion
 
-    #region Trigonometry - ChiFixed
+    #region Power
 
     [Benchmark]
-    [BenchmarkCategory("Trig", "ChiFixed")]
+    [BenchmarkCategory("Power")]
+    public ChiFixed Fixed_Pow()
+    {
+        var result = ChiFixed.Zero;
+        for (var i = 0; i < Ops; i++)
+            result = ChiFixed.Pow(ChiFixed.Abs(_fixedValues[i]) + ChiFixed.One, _fixedValues2[i] / Ten);
+        return result;
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Power")]
+    public double Double_Pow()
+    {
+        var result = 0.0;
+        for (var i = 0; i < Ops; i++)
+            result = Math.Pow(Math.Abs(_doubleValues[i]) + 1.0, _doubleValues2[i] / 10.0);
+        return result;
+    }
+
+    #endregion
+
+    #region Trigonometry
+
+    [Benchmark]
+    [BenchmarkCategory("Trig")]
     public ChiFixed Fixed_Sin()
     {
         var result = ChiFixed.Zero;
@@ -326,61 +312,7 @@ public class FixedBenchmarks
     }
 
     [Benchmark]
-    [BenchmarkCategory("Trig", "ChiFixed")]
-    public ChiFixed Fixed_Cos()
-    {
-        var result = ChiFixed.Zero;
-        for (var i = 0; i < Ops; i++)
-            result = ChiFixed.Cos(_fixedValues[i]);
-        return result;
-    }
-
-    [Benchmark]
-    [BenchmarkCategory("Trig", "ChiFixed")]
-    public ChiFixed Fixed_Tan()
-    {
-        var result = ChiFixed.Zero;
-        for (var i = 0; i < Ops; i++)
-            result = ChiFixed.Tan(_fixedValues[i]);
-        return result;
-    }
-
-    [Benchmark]
-    [BenchmarkCategory("Trig", "ChiFixed")]
-    public (ChiFixed, ChiFixed) Fixed_SinCos()
-    {
-        var result = (ChiFixed.Zero, ChiFixed.Zero);
-        for (var i = 0; i < Ops; i++)
-            result = ChiFixed.SinCos(_fixedValues[i]);
-        return result;
-    }
-
-    [Benchmark]
-    [BenchmarkCategory("Trig", "ChiFixed")]
-    public ChiFixed Fixed_Atan()
-    {
-        var result = ChiFixed.Zero;
-        for (var i = 0; i < Ops; i++)
-            result = ChiFixed.Atan(_fixedValues[i]);
-        return result;
-    }
-
-    [Benchmark]
-    [BenchmarkCategory("Trig", "ChiFixed")]
-    public ChiFixed Fixed_Atan2()
-    {
-        var result = ChiFixed.Zero;
-        for (var i = 0; i < Ops; i++)
-            result = ChiFixed.Atan2(_fixedValues[i], _fixedValues2[i]);
-        return result;
-    }
-
-    #endregion
-
-    #region Trigonometry - Double
-
-    [Benchmark]
-    [BenchmarkCategory("Trig", "Double")]
+    [BenchmarkCategory("Trig")]
     public double Double_Sin()
     {
         var result = 0.0;
@@ -390,7 +322,17 @@ public class FixedBenchmarks
     }
 
     [Benchmark]
-    [BenchmarkCategory("Trig", "Double")]
+    [BenchmarkCategory("Trig")]
+    public ChiFixed Fixed_Cos()
+    {
+        var result = ChiFixed.Zero;
+        for (var i = 0; i < Ops; i++)
+            result = ChiFixed.Cos(_fixedValues[i]);
+        return result;
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Trig")]
     public double Double_Cos()
     {
         var result = 0.0;
@@ -400,7 +342,17 @@ public class FixedBenchmarks
     }
 
     [Benchmark]
-    [BenchmarkCategory("Trig", "Double")]
+    [BenchmarkCategory("Trig")]
+    public ChiFixed Fixed_Tan()
+    {
+        var result = ChiFixed.Zero;
+        for (var i = 0; i < Ops; i++)
+            result = ChiFixed.Tan(_fixedValues[i]);
+        return result;
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Trig")]
     public double Double_Tan()
     {
         var result = 0.0;
@@ -410,7 +362,17 @@ public class FixedBenchmarks
     }
 
     [Benchmark]
-    [BenchmarkCategory("Trig", "Double")]
+    [BenchmarkCategory("Trig")]
+    public (ChiFixed, ChiFixed) Fixed_SinCos()
+    {
+        var result = (ChiFixed.Zero, ChiFixed.Zero);
+        for (var i = 0; i < Ops; i++)
+            result = ChiFixed.SinCos(_fixedValues[i]);
+        return result;
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Trig")]
     public (double, double) Double_SinCos()
     {
         var result = (0.0, 0.0);
@@ -420,7 +382,17 @@ public class FixedBenchmarks
     }
 
     [Benchmark]
-    [BenchmarkCategory("Trig", "Double")]
+    [BenchmarkCategory("Trig")]
+    public ChiFixed Fixed_Atan()
+    {
+        var result = ChiFixed.Zero;
+        for (var i = 0; i < Ops; i++)
+            result = ChiFixed.Atan(_fixedValues[i]);
+        return result;
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Trig")]
     public double Double_Atan()
     {
         var result = 0.0;
@@ -430,7 +402,17 @@ public class FixedBenchmarks
     }
 
     [Benchmark]
-    [BenchmarkCategory("Trig", "Double")]
+    [BenchmarkCategory("Trig")]
+    public ChiFixed Fixed_Atan2()
+    {
+        var result = ChiFixed.Zero;
+        for (var i = 0; i < Ops; i++)
+            result = ChiFixed.Atan2(_fixedValues[i], _fixedValues2[i]);
+        return result;
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Trig")]
     public double Double_Atan2()
     {
         var result = 0.0;
@@ -441,10 +423,10 @@ public class FixedBenchmarks
 
     #endregion
 
-    #region Exp/Log - ChiFixed
+    #region ExpLog
 
     [Benchmark]
-    [BenchmarkCategory("ExpLog", "ChiFixed")]
+    [BenchmarkCategory("ExpLog")]
     public ChiFixed Fixed_Exp()
     {
         var result = ChiFixed.Zero;
@@ -454,21 +436,7 @@ public class FixedBenchmarks
     }
 
     [Benchmark]
-    [BenchmarkCategory("ExpLog", "ChiFixed")]
-    public ChiFixed Fixed_Log()
-    {
-        var result = ChiFixed.Zero;
-        for (var i = 0; i < Ops; i++)
-            result = ChiFixed.Log(ChiFixed.Abs(_fixedValues[i]) + ChiFixed.One);
-        return result;
-    }
-
-    #endregion
-
-    #region Exp/Log - Double
-
-    [Benchmark]
-    [BenchmarkCategory("ExpLog", "Double")]
+    [BenchmarkCategory("ExpLog")]
     public double Double_Exp()
     {
         var result = 0.0;
@@ -478,7 +446,17 @@ public class FixedBenchmarks
     }
 
     [Benchmark]
-    [BenchmarkCategory("ExpLog", "Double")]
+    [BenchmarkCategory("ExpLog")]
+    public ChiFixed Fixed_Log()
+    {
+        var result = ChiFixed.Zero;
+        for (var i = 0; i < Ops; i++)
+            result = ChiFixed.Log(ChiFixed.Abs(_fixedValues[i]) + ChiFixed.One);
+        return result;
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("ExpLog")]
     public double Double_Log()
     {
         var result = 0.0;
@@ -493,7 +471,7 @@ public class FixedBenchmarks
 
     [Benchmark]
     [BenchmarkCategory("Conversion")]
-    public ChiFixed Conversion_DecimalToFixed()
+    public ChiFixed Fixed_FromDecimal()
     {
         var result = ChiFixed.Zero;
         for (var i = 0; i < Ops; i++)
@@ -503,7 +481,17 @@ public class FixedBenchmarks
 
     [Benchmark]
     [BenchmarkCategory("Conversion")]
-    public decimal Conversion_FixedToDecimal()
+    public double Double_FromDecimal()
+    {
+        var result = 0.0;
+        for (var i = 0; i < Ops; i++)
+            result = (double)(decimal)_doubleValues[i];
+        return result;
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Conversion")]
+    public decimal Fixed_ToDecimal()
     {
         var result = 0m;
         for (var i = 0; i < Ops; i++)
@@ -511,38 +499,13 @@ public class FixedBenchmarks
         return result;
     }
 
-    #endregion
-
-    #region Parsing/Formatting
-
-    private string[] _stringValues = null!;
-
-    [GlobalSetup(Target = nameof(Fixed_Parse))]
-    public void SetupParsing()
-    {
-        Setup();
-        _stringValues = new string[Ops];
-        for (var i = 0; i < Ops; i++)
-            _stringValues[i] = _fixedValues[i].ToString();
-    }
-
     [Benchmark]
-    [BenchmarkCategory("Parsing")]
-    public ChiFixed Fixed_Parse()
+    [BenchmarkCategory("Conversion")]
+    public decimal Double_ToDecimal()
     {
-        var result = ChiFixed.Zero;
+        var result = 0m;
         for (var i = 0; i < Ops; i++)
-            result = ChiFixed.Parse(_stringValues[i]);
-        return result;
-    }
-
-    [Benchmark]
-    [BenchmarkCategory("Formatting")]
-    public string Fixed_ToString()
-    {
-        var result = string.Empty;
-        for (var i = 0; i < Ops; i++)
-            result = _fixedValues[i].ToString();
+            result = (decimal)_doubleValues[i];
         return result;
     }
 
